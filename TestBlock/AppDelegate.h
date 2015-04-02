f314425d4c3746447db7f2ca3670e653c403c3e4
Leo <leo.hsu@qlync.com>
2015-04-02 10:18:36 +0800

    Initial Commit

---
+++ TestBlock/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestBlock
+//
+//  Created by Leo on 2015/4/2.
+//  Copyright (c) 2015年 leotest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestBlock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestBlock
+//
+//  Created by Leo on 2015/4/2.
+//  Copyright (c) 2015年 leotest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
